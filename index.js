f491beba0712a0106bef9cf544a82712cdeed09c
Michael <edelsteinmichael23@gmail.com>
2021-11-01 15:58:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-
-function saturdayFun(activity) {
-    console.log(`This Saturday, I want to ${activity}!`);
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
 } 
-saturdayFun();
+
+
+function mondayWork(activity2 = "go to the office") {
+    return `This Monday, I will ${activity2}.`
+}
+
+function wrapAdjective() {
+    let emphatic = result("*a hard worker*")
+        return `You are ${emphatic}!`
+    
+}
